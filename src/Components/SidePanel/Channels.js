22822c5f05178d3c9f25b8681adb5c46a0463593
saad277 <saadsheikh27@gmail.com>
2021-02-22 11:47:49 +0500

    channels created firebase

--- src/Components/SidePanel/Channels.js
+++ src/Components/SidePanel/Channels.js
@@ -1,15 +1,60 @@
 import React, { useState } from "react";
 import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+import firebase from "../../firebase";
 
-const Channels = () => {
+const CHANNEL_REF = firebase.database().ref("channels");
+
+const Channels = (props) => {
+  const { currentUser } = props;
+
   const [channels, setChannel] = useState([]);
   const [modal, setModal] = useState(false);
   const [channelName, setChannelName] = useState("");
   const [channelDetails, setChannelDetails] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (isFormValid()) {
+      console.log("channel added");
+      console.log(channelDetails, channelDetails);
+
+      addChannel();
+    }
+  };
+
+  const isFormValid = () => {
+    return channelName && channelDetails;
+  };
+
+  const addChannel = () => {
+    const key = CHANNEL_REF.push().key;
+
+    let newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: currentUser.displayName,
+        avatar: currentUser.email,
+      },
+    };
+
+    CHANNEL_REF.child(key)
+      .update(newChannel)
+      .then(() => {
+        setChannel("");
+        setChannelDetails("");
+        setModal(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
-      <Menu.Menu style={{ paddingBottom: "2em" }}>
+      <Menu.Menu>
         <Menu.Item>
           <span>
             <Icon name="exchange" /> CHANNELS
@@ -22,13 +67,14 @@ const Channels = () => {
       <Modal basic open={modal}>
         <Modal.Header>Add a Channel</Modal.Header>
         <Modal.Content>
-          <Form>
+          <Form style={{ paddingBottom: "2em" }} onSubmit={handleSubmit}>
             <Form.Field>
               <Input
                 fluid
                 label="Name of Channel"
                 name="channelName"
                 onChange={(e) => setChannelName(e.target.value)}
+                style={{ marginBottom: "2rem" }}
               ></Input>
               <Input
                 fluid
@@ -40,7 +86,7 @@ const Channels = () => {
           </Form>
         </Modal.Content>
         <Modal.Actions>
-          <Button color="green" inverted>
+          <Button color="green" inverted onClick={handleSubmit}>
             <Icon name="checkmark" /> Add
           </Button>
           <Button color="red" inverted onClick={() => setModal(false)}>
--- src/Components/SidePanel/SidePanel.js
+++ src/Components/SidePanel/SidePanel.js
@@ -14,7 +14,7 @@ const SidePanel = (props) => {
       style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
     >
       <UserPanel {...props} />
-      <Channels />
+      <Channels {...props} />
     </Menu>
   );
 };
